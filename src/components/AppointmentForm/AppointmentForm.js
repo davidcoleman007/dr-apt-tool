44ce71ccff5c4090c9fc0f2cee28701a8379b0c9
David Kenneth Coleman <david_coleman_007@hotmail.com>
2018-05-26 16:13:18 -0400

    initial project code

---
+++ src/components/AppointmentForm/AppointmentForm.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+import DatePicker from 'react-date-picker';
+
+export default class AppointmentForm extends Component {
+  render() {
+    return (
+      <div>
+        <DatePicker/>
+      </div>
+    )
+  }
+}

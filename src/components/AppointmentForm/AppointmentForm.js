e70cb16284cce4b4d061dd665d1d2c3c08dd71c2
David Kenneth Coleman <david_coleman_007@hotmail.com>
2018-05-26 17:23:29 -0400

    initial component structure

--- src/components/AppointmentForm/AppointmentForm.js
+++ src/components/AppointmentForm/AppointmentForm.js
@@ -1,11 +1,44 @@
 import React, { Component } from 'react'
-import DatePicker from 'react-date-picker';
+import ReactCalendar from 'react-calendar';
 
 export default class AppointmentForm extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      start : new Date(),
+      end: new Date(),
+    }
+    this.onChange = this.onChange.bind(this);
+  }
+
+  onChange(event) {
+    console.log(event);
+  }
+
   render() {
+    const {start, end} = this.state;
     return (
       <div>
-        <DatePicker/>
+        <article>
+          <header>Pick Appointment Date</header>
+          <ReactCalendar
+            value={start}
+            onChange={this.onStartChange}
+            showNavigation
+            showWeekNumbers
+          />
+        </article>
+        <article>
+          <header>Pick Appointment Time</header>
+          <section>
+            <label>Start</label>
+            <input type="time" />
+          </section>
+          <section>
+            <label>End</label>
+            <input type="time" />
+          </section>
+        </article>
       </div>
     )
   }
